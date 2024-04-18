081cf30d46859f650a669110407e7d8bbfd047b1
Haggai Khafizov <hafyok5777@gmail.com>
2024-04-18 16:08:24 +0300

    Test stats screen with random data

--- app/src/main/java/com/example/diplomtest/presentation/StatsScreen/StatsContent.kt
+++ app/src/main/java/com/example/diplomtest/presentation/StatsScreen/StatsContent.kt
@@ -1,10 +1,10 @@
 package com.example.diplomtest.presentation.StatsScreen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,10 +25,75 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlin.random.Random
+import co.yml.charts.axis.AxisData
+import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.IntersectionPoint
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
+import co.yml.charts.ui.linechart.model.LineStyle
+import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
+import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
+import co.yml.charts.ui.linechart.model.ShadowUnderLine
+import com.example.diplomtest.ui.theme.DiplomTestTheme
+import co.yml.charts.ui.linechart.LineChart
+
+
 
 //@Preview
 @Composable
 fun StatsContent(navController: NavController) {
+    val steps = 10
+
+    val pointsList = getPointsList()
+    val max = getMax(pointsList)
+    val min = getMin(pointsList)
+    Log.d("MyLog", "Max: $max Min: $min")
+    val xAxisData = AxisData.Builder()
+        .axisStepSize(100.dp)
+        .backgroundColor(Color.Transparent)
+        .steps(pointsList.size - 1)
+        .labelData { i -> i.toString() + "d" }
+        .labelAndAxisLinePadding(15.dp)
+        .build()
+
+    val yAxisData = AxisData.Builder()
+        .steps(steps)
+        .backgroundColor(Color.Transparent)
+        .labelAndAxisLinePadding(20.dp)
+        .labelData { i ->
+            val yScale = (max - min) / steps.toFloat()
+            String.format("%.1f", ((i * yScale) + min))
+        }.build()
+    DiplomTestTheme {
+        val lineChartData = LineChartData(
+            linePlotData = LinePlotData(
+                lines = listOf(
+                    Line(
+                        dataPoints = pointsList,
+                        LineStyle(color = Color.Green, width = 1.0f),
+                        IntersectionPoint(color = Color.Blue, radius = 3.dp),
+                        SelectionHighlightPoint(),
+                        ShadowUnderLine(),
+                        SelectionHighlightPopUp()
+                    )
+                ),
+            ),
+            xAxisData = xAxisData,
+            yAxisData = yAxisData,
+            gridLines = GridLines(),
+            backgroundColor = Color.White
+        )
+
+        LineChart(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
+            lineChartData = lineChartData
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -36,49 +101,41 @@ fun StatsContent(navController: NavController) {
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Statistics",
-            style = TextStyle(
-                fontFamily = FontFamily.SansSerif,
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            ),
-            modifier = Modifier.padding(vertical = 16.dp)
-        )
 
-        Spacer(modifier = Modifier.height(16.dp))
 
-        NumberList()
-
-        CategoriesDropdown()
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        StatisticCard(
-            title = "Stat 1",
-            data = listOf(10, 20, 30, 25, 35),
-            color = Color(0xFF0077C0)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        StatisticCard(
-            title = "Stat 2",
-            data = listOf(40, 35, 45, 50, 55),
-            color = Color(0xFF00A080)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        StatisticCard(
-            title = "Stat 3",
-            data = listOf(15, 10, 20, 30, 40),
-            color = Color(0xFF9B59B6)
-        )
     }
 }
 
+fun getPointsList(): List<co.yml.charts.common.model.Point> {
+    val list = ArrayList<co.yml.charts.common.model.Point>()
+    for (i in 0..31) {
+        list.add(
+            co.yml.charts.common.model.Point(
+                i.toFloat(),
+                Random.nextInt(50, 90).toFloat()
+            )
+        )
+    }
+    return list
+}
+
+private fun getMax(list: List<co.yml.charts.common.model.Point>): Float{
+    var max = 0F
+    list.forEach { point ->
+        if(max < point.y) max = point.y
+    }
+    return max
+}
+
+private fun getMin(list: List<co.yml.charts.common.model.Point>): Float{
+    var min = 100F
+    list.forEach { point ->
+        if(min > point.y) min = point.y
+    }
+    return min
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 @Composable
 fun StatisticCard(title: String, data: List<Int>, color: Color) {
     Card(
