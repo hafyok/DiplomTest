1a632093ecaa89d6afd289807a04b04df8d4ad6a
Haggai Khafizov <hafyok5777@gmail.com>
2024-05-20 16:28:32 +0300

    Edit strings

--- app/src/main/java/com/example/diplomtest/MyForegroundService.kt
+++ app/src/main/java/com/example/diplomtest/MyForegroundService.kt
@@ -1,5 +1,6 @@
 package com.example.diplomtest
 
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -11,6 +12,7 @@ import android.os.IBinder
 import androidx.core.app.NotificationCompat
 
 
+@Suppress("DEPRECATION")
 class MyForegroundService : Service() {
 
     override fun onCreate() {
@@ -35,10 +37,11 @@ class MyForegroundService : Service() {
         notificationManager.cancel(1)
     }
 
+    @SuppressLint("ObsoleteSdkInt")
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                "my_channel_id",
+                getString(R.string.warning),
                 "My Foreground Service",
                 NotificationManager.IMPORTANCE_DEFAULT
             ).apply {
@@ -59,10 +62,10 @@ class MyForegroundService : Service() {
             PendingIntent.FLAG_IMMUTABLE // Добавление флага FLAG_IMMUTABLE
         )
 
-        return NotificationCompat.Builder(this, "my_channel_id")
-            .setContentTitle("My Foreground Service")
-            .setContentText("Service is running")
-            .setSmallIcon(R.drawable.play_icon)
+        return NotificationCompat.Builder(this, getString(R.string.warning))
+            .setContentTitle(getString(R.string.you_out))
+            .setContentText(getString(R.string.please_back))
+            .setSmallIcon(R.drawable.play_icon) //TODO() заменить иконку
             .setContentIntent(pendingIntent)
             .build()
     }
