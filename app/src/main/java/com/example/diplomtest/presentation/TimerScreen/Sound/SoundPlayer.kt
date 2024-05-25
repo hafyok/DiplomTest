062142f6ea3a2a411c38ec44fdc334b31f313a73
Haggai Khafizov <hafyok5777@gmail.com>
2024-05-25 19:28:19 +0300

    The pictures are changing

--- app/src/main/java/com/example/diplomtest/presentation/TimerScreen/Sound/SoundPlayer.kt
+++ app/src/main/java/com/example/diplomtest/presentation/TimerScreen/Sound/SoundPlayer.kt
@@ -1,5 +1,6 @@
 package com.example.diplomtest.presentation.TimerScreen.Sound
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,35 +8,39 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diplomtest.R
 
 @Composable
 fun SoundPlayer(viewModel: SoundPlayerViewModel) {
+    //val iconMusic by viewModel.iconMusic.observeAsState(R.drawable.genos)
 
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
-
-        /*Image(
-            painter = painterResource(id = R.drawable.ic_music),
-            contentDescription = "",
-            modifier = Modifier
-                .height(160.dp)
-                .width(160.dp)
-                .padding(32.dp)
-                .background(Color.White)
-        )*/
         Column() {
+            Column {
+                Image(
+                    painter = painterResource(id = viewModel.iconMusic),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(150.dp) // Установите нужный размер
+                        .clip(RoundedCornerShape(32.dp)) // Скругление краев
+                )
+            }
             Row() {
 
                 IconButton(onClick = { viewModel.audioStart() }, modifier = Modifier.size(35.dp)) {
@@ -47,8 +52,7 @@ fun SoundPlayer(viewModel: SoundPlayerViewModel) {
 
                 IconButton(onClick = { viewModel.audioPause() }, modifier = Modifier.size(35.dp)) {
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_pause),
-                        contentDescription = ""
+                        painter = painterResource(id = R.drawable.ic_pause), contentDescription = ""
                     )
                 }
             }
@@ -58,17 +62,25 @@ fun SoundPlayer(viewModel: SoundPlayerViewModel) {
                 Text(text = "LoFi",
                     Modifier
                         .padding(vertical = 4.dp)
-                        .clickable { viewModel.changeAudioLofi()})
+                        .clickable { viewModel.changeAudioLofi() })
                 Text(text = "White Noises",
                     Modifier
                         .padding(vertical = 4.dp)
-                        .clickable { viewModel.changeAudioWhiteNoise()})
+                        .clickable { viewModel.changeAudioWhiteNoise() })
                 Text(text = "Binaural beats",
                     Modifier
                         .padding(vertical = 4.dp)
-                        .clickable { viewModel.changeAudioBeats()})
+                        .clickable { viewModel.changeAudioBeats() })
             }
         }
 
     }
+}
+
+@Composable
+@Preview
+fun PreviewSoundPlayer() {
+    val context = LocalContext.current
+    val viewModel = SoundPlayerViewModel(context)
+    SoundPlayer(viewModel = viewModel)
 }
--- app/src/main/java/com/example/diplomtest/presentation/TimerScreen/Sound/SoundPlayerViewModel.kt
+++ app/src/main/java/com/example/diplomtest/presentation/TimerScreen/Sound/SoundPlayerViewModel.kt
@@ -2,12 +2,17 @@ package com.example.diplomtest.presentation.TimerScreen.Sound
 
 import android.content.Context
 import android.media.MediaPlayer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.diplomtest.R
 
 //TODO() проверить утечку памяти (private val context: Context)
 class SoundPlayerViewModel(private val context: Context): ViewModel() {
     private var mp: MediaPlayer = MediaPlayer.create(context, R.raw.lofi)
+    var iconMusic by mutableIntStateOf(R.drawable.lofi_girl)
+        private set
 
     fun audioStart(){
         mp.start()
@@ -20,18 +25,21 @@ class SoundPlayerViewModel(private val context: Context): ViewModel() {
     fun changeAudioWhiteNoise(){
         audioPause()
         mp = MediaPlayer.create(context, R.raw.white_noise)
+        iconMusic = R.drawable.white_noise
         audioStart()
     }
 
     fun changeAudioLofi(){
         audioPause()
         mp = MediaPlayer.create(context, R.raw.lofi)
+        iconMusic = R.drawable.lofi_girl
         audioStart()
     }
 
     fun changeAudioBeats(){
         audioPause()
         mp = MediaPlayer.create(context, R.raw.binaural_beats_40hz)
+        iconMusic = R.drawable.binaural_beats
         audioStart()
     }
 
